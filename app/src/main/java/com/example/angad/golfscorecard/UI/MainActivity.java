fe608a51c8d84d8ff3f55aebf9940f54dd34e3ae
Angad Singh <angad.dtu2012@gmail.com>
2016-08-25 23:29:33 +0530

    Initial commit

---
+++ app/src/main/java/com/example/angad/golfscorecard/UI/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.angad.golfscorecard.UI;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.angad.golfscorecard.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/angad/golfscorecard/model/Hole.java
@@ -0,0 +1,22 @@
+package com.example.angad.golfscorecard.model;
+
+public class Hole {
+    private int mHoleNumber;
+    private int mCount;
+
+    public int getHoleNumber() {
+        return mHoleNumber;
+    }
+
+    public void setHoleNumber(int holeNumber) {
+        mHoleNumber = holeNumber;
+    }
+
+    public int getCount() {
+        return mCount;
+    }
+
+    public void setCount(int count) {
+        mCount = count;
+    }
+}
